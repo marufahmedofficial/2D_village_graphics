c2b358ebbbb7dbabbaf435da7e7a8588922d5b6b
Maruf Ahmed <mk6116055@gmail.com>
2023-04-10 03:31:47 +0600

    first commit

---
+++ 2D_village_graphics.cpp
@@ -0,0 +1,19 @@
+#include<graphics.h>
+#include<stdio.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char*)"");
+    line(200,200,300,200);
+    line(200,100,200,200);
+    line(300,100,300,200);
+    line(200,100,300,100);
+    line(200,100,250,50);
+    line(250,50,300,100);
+
+    getch();
+    closegraph();
+    return 0;
+}
+
